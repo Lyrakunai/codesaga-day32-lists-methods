b918503fc4b6cce4d23f0061695dfa2421f4635e
Lyrakun <273812235+Lyrakunai@users.noreply.github.com>
2026-05-12 12:50:34 +0530

    Add files via upload

---
+++ day32_challenges.py
@@ -0,0 +1,17 @@
+# Challenge 1
+# Library System
+books = ["Math", "Science", "English", "History"]
+books.append("Computer")
+books.insert(2, "Hindi")
+books.remove("Math")
+last_book = books.pop()
+print(books)
+print(last_book)
+
+
+# Challenge 2
+# Duplicate Trap
+items = ["pen", "book", "book", "pencil", "book"]
+items.remove("book")
+items.pop(1)
+print(items)
---
+++ day32_practice.py
@@ -0,0 +1,30 @@
+# append()
+x = [10, 20]
+x.append(30)
+print(x)
+
+# insert()
+x = [10, 20]
+x.insert(1, 99)
+print(x)
+
+# remove()
+x = ["a", "b", "c"]
+x.remove("b")
+print(x)
+
+# pop()
+x = ["a", "b", "c"]
+x.pop(0)
+print(x)
+
+# pop()
+x = [10,20,30]
+y = x.pop()
+print(y)
+print(x)
+
+# duplicate value check
+x = [1, 2, 2, 3]
+x.remove(2)
+print(x)
